fabffaa29d71eb4d5caf0b4470f31685294b1feb
kota oue <kotaoue@gmail.com>
2022-03-22 22:25:32 +0900

    add orange theme

--- pkg/portrait/theme.go
+++ pkg/portrait/theme.go
@@ -21,6 +21,7 @@ const (
 	PinkPurpleTheme = "pinkPurple"
 	PinkTheme       = "pink"
 	RedTheme        = "red"
+	OrangeTheme     = "orange"
 )
 
 type Theme struct{}
@@ -57,6 +58,8 @@ func (t Theme) Get(theme string) ([]color.Color, error) {
 		return t.pink(), nil
 	case RedTheme:
 		return t.red(), nil
+	case OrangeTheme:
+		return t.orange(), nil
 	}
 	return nil, errors.New("not exist theme")
 }
@@ -307,6 +310,23 @@ func (Theme) red() []color.Color {
 		color.RGBA{158, 78, 73, 255},   // メインカラー 影
 		color.RGBA{235, 33, 18, 255},   // サブカラー
 		color.RGBA{158, 78, 73, 255},   // サブカラー 影
+		color.RGBA{255, 0, 0, 255},     // トサカ
+		color.RGBA{255, 128, 128, 255}, // トサカ ハイライト
+		color.RGBA{196, 0, 0, 255},     // トサカ 影
+		color.RGBA{255, 255, 0, 255},   // くちばし
+		color.RGBA{255, 255, 255, 255}, // くちばし ハイライト
+		color.RGBA{255, 255, 0, 255},   // 足
+	}
+}
+
+func (Theme) orange() []color.Color {
+	return []color.Color{
+		color.RGBA{0, 0, 0, 0},         // 背景色
+		color.RGBA{0, 0, 0, 255},       // 主線
+		color.RGBA{255, 120, 29, 255},  // メインカラー
+		color.RGBA{178, 103, 79, 255},  // メインカラー 影
+		color.RGBA{255, 120, 29, 255},  // サブカラー
+		color.RGBA{178, 103, 79, 255},  // サブカラー 影
 		color.RGBA{255, 0, 0, 255},     // トサカ
 		color.RGBA{255, 128, 128, 255}, // トサカ ハイライト
 		color.RGBA{196, 0, 0, 255},     // トサカ 影
